47d6195266660d35d3cd4571180762ed75ed6ffb
John Hamlin <johnhamlin@gmail.com>
2022-11-08 11:19:21 -0500

    Complete first 5 problems

--- homework.js
+++ homework.js
@@ -1,18 +1,25 @@
 // 1. Set the variable `givenName` to the string "Addison".
+const givenName = 'Addison';
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+const candies = 20;
+const people = 6;
+const leftover = 20 % 6;
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+const greeting = name => `Hello, ${name}!`;
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+const isOdd = n => n % 2 === 1 || n % 2 === -1;
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+const isEven = n => n % 2 === 0;
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
